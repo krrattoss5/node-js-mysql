f009007f06281732723d60af0fc4c61f4c8ec5f5
krrattoss5 <krrattoss5@gmail.com>
2023-10-31 19:49:08 -0500

    tercer commit

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,6 +1,15 @@
 import {pool} from '../db.js'
 
-export const getEmployees = (req,res)=>res.send('Obteniendo empleados...')
+export const getEmployees = async (req,res)=>{
+  const [rows] = await pool.query('SELECT * FROM employee')
+  res.json(rows)
+}
+
+export const getEmployee = async (req,res)=>{
+  const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?',[req.params.id])
+
+  rows.length <= 0?res.status(404).json({"employee":"Not Found!"}):res.json(rows[0])
+}
 
 export const createEmployee = async (req,res)=>{
   const {name,salary} = req.body
@@ -14,4 +23,8 @@ export const createEmployee = async (req,res)=>{
 
 export const updateEmployee = (req,res)=>res.send('Actualizando empleados...')
 
-export const deleteEmployee = (req,res)=>res.send('Eliminando empleados...')
+export const deleteEmployee = async (req,res)=>{
+  const [result] = await pool.query('DELETE FROM employee WHERE id = ?',[req.params.id])
+
+  result.affectedRows <= 0?res.status(404).json({"employee":"Not Exist!"}):res.status(204).json({"employee":"Deleted"})
+}
--- src/routes/employes.routes.js
+++ src/routes/employes.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {deleteEmployee, getEmployees, updateEmployee, createEmployee} from "../controllers/employees.controller.js"
+import {deleteEmployee, getEmployees, updateEmployee, createEmployee, getEmployee} from "../controllers/employees.controller.js"
 
 const route = Router()
 
 route.get('/employees',getEmployees)
+route.get('/employees/:id',getEmployee)
 route.post('/employees',createEmployee)
 route.put('/employees',updateEmployee)
-route.delete('/employees',deleteEmployee)
+route.delete('/employees/:id',deleteEmployee)
 
 export default route;
